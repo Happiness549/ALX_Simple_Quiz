1660cc7d772988dd787e2bd714211e53b43f7d56
Happiness Mhlongo <mhlongohappiness880@gmail.com>
2025-08-04 10:14:18 +0200

    Add corrected answer message

--- quiz.js
+++ quiz.js
@@ -1,12 +1,15 @@
-'strict mode';
+
 
 function checkAnswer(){
     const correctAnswer = "4";
     const selectedOption = document.querySelector('input[name ="quiz"]:checked');
     const userAnswer = selectedOption ? selectedOption.value : null;
-    
+
+
+     const feedback = document.getElementById('feedback');
+
    if(userAnswer === correctAnswer){
-    Feedback.textContent = "Correct! well done."
+    feedback.textContent = "Correct! well done."
    }else{
     feedback.textContent = "That's incorrect. Try again!"
    }
