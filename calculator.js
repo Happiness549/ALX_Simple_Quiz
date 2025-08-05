99d0eed9c305066e6dcf477731d59f0e2c086990
Happiness Mhlongo <mhlongohappiness880@gmail.com>
2025-08-05 16:08:05 +0200

    Rectify subtracting function

--- calculator.js
+++ calculator.js
@@ -28,13 +28,13 @@ document.getElementById('add').addEventListener("click", function(){
 });
 
 document.getElementById('subtract').addEventListener('click', function(){
-        const number1 = parseFloat(document.getElementById('subtract').value) || 0;
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
     const number2 = parseFloat(document.getElementById('number2').value) || 0;
     const result = subtract(number1, number2);
      document.getElementById('calculation-result').textContent = result;
 });
 
-document.getElementById('divide', function(){
+document.getElementById('divide').addEventListener('click', function(){
     const number1 = parseFloat(document.getElementById('number1').value) || 0;
     const number2 = parseFloat(document.getElementById('number2').value) || 0;
     const result = divide(number1, number2);
